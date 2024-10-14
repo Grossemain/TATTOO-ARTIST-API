7cb6465393e43048ef042f3e852d2f11ed7aea10
Grossemain <grossemain@gmail.com>
2024-10-14 18:30:39 +0200

    Modification methode SearchByUserAndArtStyle avec ajout de recherche par description

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -73,6 +73,7 @@ class UserController extends Controller
         $users = User::where(function ($query) use ($search) {
             $query->where('city', 'like', '%' . $search . '%')
             ->orWhere('pseudo_user', 'like', '%' . $search . '%')
+            ->orWhere('description', 'like',  '%' . $search . '%')
                   ->orWhere('departement', 'like', '%' . $search . '%')
                   ->orWhereHas('artstyles', function ($query) use ($search) {
                       $query->where('name', 'like', '%' . $search . '%');
