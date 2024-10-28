7bc54fa102345fba76364895ef7345d0dd684f7b
Grossemain <grossemain@gmail.com>
2024-10-28 16:30:36 +0100

    suppression ligne creation user factory dans UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -32,6 +32,6 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
             ],
         ]);
-        User::factory(1)->create();
+        // User::factory(1)->create();
     }
 }
