a584907fc0b5477a6a68b52bb12db6ff4348fd05
Grossemain <grossemain@gmail.com>
2024-10-29 11:38:17 +0100

    roll back commit précédent

--- database/migrations/1_create_users_table.php
+++ database/migrations/1_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('user_id');
             $table->unsignedBigInteger('role_id')->default(1); 
-            $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
